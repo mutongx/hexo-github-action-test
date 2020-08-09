c42941feb7b091acfaf7851580ca88321498b997
Mutong Xie <mutong.xie@tusimple.ai>
2020-08-10 02:20:13 +0800

    Add a simple script to strip out slug in image reference

---
+++ scripts/strip-slug-dir.js
@@ -0,0 +1,8 @@
+// Not sure why hexo insist on such a stupid directory reference. So disappointed.
+// This regular expression cannot handle all conditions correctly. Just don't put square brackets in your alt text.
+hexo.extend.filter.register('before_post_render', function(data) {
+  var exp = '!\\[(.*?)\\]\\(' + data.slug + '/(.*?)\\)'
+  var re = new RegExp(exp, 'g')
+  data.content = data.content.replace(re, '![$1]($2)')
+  return data
+});
